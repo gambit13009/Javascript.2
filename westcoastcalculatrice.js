927d9b74324f7a71edf1277d87db309748064ab6
Votre nom ou pseudo <maxime.malafosse@gmail.com>
2018-05-22 14:34:32 +0200

    readme

---
+++ westcoastcalculatrice.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+  <link rel='stylesheet' type='text/css' href='style.css'>
+  <title>calculette</title>
+</head>
+<body>
+<h1> westcoastcalculator </h1>
+</body>
+
