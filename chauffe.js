94c5b900eb280f2398cd1d8b069a9d30c925e2c6
Votre nom ou pseudo <maxime.malafosse@gmail.com>
2018-05-22 12:23:47 +0200

    coucou

---
+++ chauffe.js
@@ -0,0 +1,19 @@
+function my_max1(){
+var numbers = [1,2,3,4,5,6,7,8];
+return Math.my_max1(numbers);
+}
+alert(my_max1());
+
+
+function vowel_count2(){
+var hibou = prompt("Veuillez entrer votre texte pour compter les voyelles");
+return (hibou.match(/[aeiouy]/ig)||[]).length;
+}
+alert(vowel_count2());
+
+
+function alenvers3() {
+var pourfaireparlerlescons = prompt("Veuillez entrer votre phrase à inverser");
+return pourfaireparlerlescons.split("").reverse().join("");
+}
+alert(alenvers3());
